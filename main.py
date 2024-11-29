48230e7e332f85be834e1219d8a0cd04bebdaeed
mohibur-nabil <nabil.rahman@genweb2.com>
2024-11-29 17:50:57 +0600

    updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import utils
 
 image_url = input("enter the image url: ")
-url= utils.get_google_rev_search_results(image_url)
+url= utils.google_search(image_url)
 html_name=utils.html_download(url)
 text=utils.extract_text_from_html(html_name)
 summary=utils.get_summary(text)
